81081c43bd02334e23d9119c23f5a53697d5116d
mehdi_heydarzade <ariyaware@hotmail.com>
2019-11-30 16:00:01 +0330

    full with firebase

---
+++ app/src/main/java/com/scotech/MainApplication.java
@@ -0,0 +1,66 @@
+package com.scotech;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+//firebase
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
+//firebase
+
+
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import cl.json.RNSharePackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+      new MainReactPackage(),
+      //firebase
+      new RNFirebasePackage(),
+      new RNFirebaseMessagingPackage(),
+      new RNFirebaseNotificationsPackage(),
+      //end
+      new LinearGradientPackage(),
+      new RNGestureHandlerPackage(),
+      new RNSharePackage(),
+      new ReactNativePushNotificationPackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
